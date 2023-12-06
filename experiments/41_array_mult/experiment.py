fa4ad1c78e5bd5b05a080862e7f5a8cfa617cd36
Jonas Schiller <q533604@europe.bmw.corp>
2023-12-06 15:30:15 +0100

    Add Experiment

---
+++ experiments/41_array_mult/experiment.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python3
+
+import sys
+import os.path
+import numpy as np
+np.set_printoptions(threshold=np.inf)
+np.set_printoptions(linewidth=np.inf)
+
+print("\n----Python unconcealed computation start----\n")
+
+datatype = int
+
+# count players
+player = 0
+while os.path.isfile("Player-Data/Input-P" + str(player) +"-0"):
+    player += 1
+
+if player == 0:
+    exit()
+
+# count inputs
+f = open("Player-Data/Input-P0-0", 'r')
+size = len(f.readline().split())
+
+inputs = np.zeros( (player, size), dtype=datatype)
+# fill player inputs
+player = 0
+while os.path.isfile("Player-Data/Input-P" + str(player) +"-0"):
+    f = open("Player-Data/Input-P" + str(player) +"-0", 'r')
+    inputs[player] = np.array(f.readline().split())
+    player += 1
+
+result = inputs[0]
+for i in range(1, len(inputs)):
+    result = np.multiply(result, inputs[i])
+
+print([x for x in result])
+
+print("\n----Python unconcealed computation stop----\n")
