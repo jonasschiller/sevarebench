a556f9ffe02cdfbc76eb3b6549cf5ae20a5e7446
Jonas Schiller <q533604@europe.bmw.corp>
2023-12-11 15:25:05 +0100

    Change verify

--- experiments/54_Bit_XOR/verify.py
+++ experiments/54_Bit_XOR/verify.py
@@ -1,12 +1,22 @@
-#! /usr/bin/python3
+import re
 import numpy as np
 import sys
 
-precision = 0.1
 
-# move values to array, expected as arguments
-result_is = np.array([float(i) for i in sys.argv[1].split(",")])
-result_expected= np.array([float(i) for i in sys.argv[2].split()])
+def load_results(path):
+    f = open(path,'r')
+    for line in f:
+        if line[0] == '[' and line[len(line)-2] == ']':
+            # convert string to np array
+            return np.array([int(x) for x in re.split('  |, | ',line[1:-2])])
+    print("Value not found error " + path)
+    exit()
 
-print(1) if np.allclose(result_is,result_expected,0,precision) else print(0)
-#could also do symmetric difference to find the differing values
+# load inputs
+r_is = load_results(sys.argv[1])
+r_expected = load_results(sys.argv[2])
+
+# find symmetric set difference
+diff = np.setdiff1d(r_is, r_expected)
+print(1) if len(diff) == 0 \
+    else print("Verify error: " + str(r_is[:17]) + "(...) != " + str(r_expected[:17]) + "(...)")
