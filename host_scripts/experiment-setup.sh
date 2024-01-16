17e73301c95b5804432466994acc358eb1f05efc
Jonas Schiller <q533604@europe.bmw.corp>
2024-01-16 21:44:56 +0100

    Updated Topology

--- host_scripts/experiment-setup.sh
+++ host_scripts/experiment-setup.sh
@@ -11,11 +11,10 @@ REPO_DIR=$(pos_get_variable repo_dir --from-global)
 REPO2_DIR=$(pos_get_variable repo2_dir --from-global)
 EXPERIMENT=$(pos_get_variable experiment --from-global)
 # SMC protocols to compile
-protocols="$1"
-ipaddr="$2"
-SWAP="$3"
-network="$4"
-read -r -a nodes <<< "$5"
+ipaddr="$1"
+SWAP="$2"
+network="$3"
+read -r -a nodes <<< "$4"
 groupsize=${#nodes[*]}
 
 
@@ -43,53 +42,54 @@ installDriver() {
 
 nic0=$(pos_get_variable "$(hostname)"NIC0 --from-global)
 nic1=$(pos_get_variable "$(hostname)"NIC1 --from-global) || nic1=0
+nic2=$(pos_get_variable "$(hostname)"NIC2 --from-global) || nic2=0
 
 ips=()
 
-######
-### three nodes indirect connection topology setup
-### node 2 --- node 1 --- node 3
-### for 25G+ speeds set MTU
-if [ "$(hostname | grep -cE "gard|goracle|zone")" -eq 1 ]; then
-
-	# install ddp drivers
-	installDriver
-
-	# store other participants ips
-	for i in $(seq 2 $((groupsize+1))); do
-		[ "$ipaddr" -ne "$i" ] && ips+=( "$i" )
-	done
-
-	ip addr add 10.10."$network"."$ipaddr"/24 dev "$nic0"
-	ip link set dev "$nic0" mtu 9700
-	ip link set dev "$nic0" up
-
-	if [ "$ipaddr" -eq 2 ]; then
-		# activate forwarding for the center node
-		sysctl -w net.ipv4.ip_forward=1
-		ip addr add 10.10."$network"."$ipaddr"/24 dev "$nic1"
-		ip link set dev "$nic1" mtu 9700
-		ip link set dev "$nic1" up
-		# route via correct NICs
-		ip route add 10.10."$network".3 dev "$nic0"
-		ip route add 10.10."$network".4 dev "$nic1"
-	# this is probably not required since the routes should be set automatically
-	elif [ "$ipaddr" -eq 3 ]; then
-		ip route add 10.10."$network".4 via 10.10."$network".2
-	else
-		ip route add 10.10."$network".3 via 10.10."$network".2
-	fi
-# three nodes direct connection topology if true
-elif [ "$nic1" != 0 ]; then
-##### three nodes direct connection topology if true
-####if [ "$nic1" != 0 ]; then
+# four nodes direct connection topology if true
+if [ "$nic1" != 0 ] && [ "$nic2" != 0 ]; then
 
 	# to achieve high speeds, install ddp drivers
-	[ "$(hostname | grep -cE "meld|tinyman|yieldly|gard|goracle|zone")" -eq 1 ] && \
-		installDriver
+	highspeed=$(hostname | grep -cE "idex|meld|tinyman|yieldly|algofi|gard|goracle|zone")
+	[ "$highspeed" -eq 1 ] && installDriver
 
-	# verify that nodes array is circularly sorted like
-	# --nodes nodeA,nodeB,nodeC or --node nodeC,nodeA,nodeB
+	# verify that nodes array is circularly sorted
+	# this is required for the definition of this topology
+	
+	# specify the ip pair to create the network routes to
+	# it's not the ip that is being set to this host
+	[ "$ipaddr" -eq 2 ] && ips+=( 3 4 5 )
+	[ "$ipaddr" -eq 3 ] && ips+=( 4 5 2 )
+	[ "$ipaddr" -eq 4 ] && ips+=( 5 2 3 )
+	[ "$ipaddr" -eq 5 ] && ips+=( 2 3 4 )
+
+	ip addr add 10.10."$network"."$ipaddr"/24 dev "$nic0"
+	ip addr add 10.10."$network"."$ipaddr"/24 dev "$nic1"
+	ip addr add 10.10."$network"."$ipaddr"/24 dev "$nic2"
+
+	ip link set dev "$nic0" up
+	ip link set dev "$nic1" up
+	ip link set dev "$nic2" up
+
+	ip route add 10.10."$network"."${ips[0]}" dev "$nic0"
+	ip route add 10.10."$network"."${ips[1]}" dev "$nic1"
+	ip route add 10.10."$network"."${ips[2]}" dev "$nic2"
+
+	# to achieve high speeds, increase mtu
+	if [ "$highspeed" -eq 1 ]; then
+		ip link set dev "$nic0" mtu 9700
+		ip link set dev "$nic1" mtu 9700
+		ip link set dev "$nic2" mtu 9700
+	fi
+
+# three nodes direct connection topology if true
+elif [ "$nic1" != 0 ]; then
+
+	# to achieve high speeds, install ddp drivers
+	highspeed=$(hostname | grep -cE "idex|meld|tinyman|yieldly|algofi|gard|goracle|zone")
+	[ "$highspeed" -eq 1 ] && installDriver
+
+	# verify that nodes array is circularly sorted
 	# this is required for the definition of this topology
 	
 	# specify the ip pair to create the network routes to
@@ -108,10 +108,10 @@ elif [ "$nic1" != 0 ]; then
 	ip route add 10.10."$network"."${ips[1]}" dev "$nic1"
 
 	# to achieve high speeds, increase mtu
-	[ "$(hostname | grep -cE "meld|tinyman|yieldly|gard|goracle|zone")" -eq 1 ] && {
+	if [ "$(hostname | grep -cE "meld|tinyman|yieldly|gard|goracle|zone")" -eq 1 ]; then
 		ip link set dev "$nic0" mtu 9700
 		ip link set dev "$nic1" mtu 9700
-	}
+	fi
 
 # here the testhosts are connected via switch
 else
@@ -175,6 +175,29 @@ stopserver
 
 pos_upload speedtest
 
+# set up swap disk for RAM pageswapping measurements
+if [ -n "$SWAP" ] && [ -b /dev/nvme0n1 ]; then
+	echo "creating swapfile with swap size $SWAP"
+	parted -s /dev/nvme0n1 mklabel gpt
+	parted -s /dev/nvme0n1 mkpart primary ext4 0% 100%
+	mkfs.ext4 -FL swap /dev/nvme0n1
+	mkdir /swp
+	mkdir /whale
+	mount -L swap /swp
+	dd if=/dev/zero of=/swp/swp_file bs=1024 count="$SWAP"K
+	chmod 600 /swp/swp_file
+	mkswap /swp/swp_file
+	swapon /swp/swp_file
+	 # create ramdisk
+    totalram=$(free -m | grep "Mem:" | awk '{print $2}')
+	mount -t tmpfs -o size="$totalram"M swp /whale
+	# preoccupy ram and only leave 16 GiB for faster experiment runs
+	# it was observed, that more than that was never required and 
+	# falloc is slow in loops on nodes with large ram
+	ram=$((16*1024))
+	availram=$(free -m | grep "Mem:" | awk '{print $7}')
+	fallocate -l $((availram-ram))M /whale/filler
+fi
 
 #######
 #### compile libaries and prepare experiments
@@ -229,30 +252,6 @@ done
 pos_upload makelog
 # abort if no success
 $success
-
-# set up swap disk for RAM pageswapping measurements
-if [ -n "$SWAP" ] && [ -b /dev/nvme0n1 ]; then
-	echo "creating swapfile with swap size $SWAP"
-	parted -s /dev/nvme0n1 mklabel gpt
-	parted -s /dev/nvme0n1 mkpart primary ext4 0% 100%
-	mkfs.ext4 -FL swap /dev/nvme0n1
-	mkdir /swp
-	mkdir /whale
-	mount -L swap /swp
-	dd if=/dev/zero of=/swp/swp_file bs=1024 count="$SWAP"K
-	chmod 600 /swp/swp_file
-	mkswap /swp/swp_file
-	swapon /swp/swp_file
-	 # create ramdisk
-    totalram=$(free -m | grep "Mem:" | awk '{print $2}')
-	mount -t tmpfs -o size="$totalram"M swp /whale
-	# preoccupy ram and only leave 16 GiB for faster experiment runs
-	# it was observed, that more than that was never required and 
-	# falloc is slow in loops on nodes with large ram
-	ram=$((16*1024))
-	availram=$(free -m | grep "Mem:" | awk '{print $7}')
-	fallocate -l $((availram-ram))M /whale/filler
-fi
 
 echo "experiment setup successful"
 
