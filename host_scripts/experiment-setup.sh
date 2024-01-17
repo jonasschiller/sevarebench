c3e502afbee53e4bb9b5c01d4747b49dd5cf81d4
Jonas Schiller <q533604@europe.bmw.corp>
2024-01-17 08:52:52 +0100

    Fixed Syntax Error

--- host_scripts/experiment-setup.sh
+++ host_scripts/experiment-setup.sh
@@ -84,7 +84,7 @@ if [ "$nic1" != 0 ] && [ "$nic2" != 0]  && ["$groupsize" == 4]; then
 	fi
 
 # three nodes direct connection topology if true
-elif [ "$nic1" != 0 ] && ["$groupsize"==3]; then
+elif [ "$nic1" != 0 ] && ["$groupsize" == 3]; then
 
 	# to achieve high speeds, install ddp drivers
 	highspeed=$(hostname | grep -cE "idex|meld|tinyman|yieldly|algofi|gard|goracle|zone")
