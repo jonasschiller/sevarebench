7aedc71fe280b09270e2d943d7f57aecb86caa7d
Schiller Jonas <q533604@europe.bmw.corp>
2024-02-24 23:21:18 +0100

    BIT And

--- host_scripts/measurement.sh
+++ host_scripts/measurement.sh
@@ -46,7 +46,7 @@ cd "$REPO_DIR"
             RING) 
                 /bin/time -f "$timerf" ./compile.py -R 128 experiment "$size" "$partysize" "$etype";;
             BINARY) 
-                /bin/time -f "$timerf" ./compile.py -B 32 experiment "$size" "$partysize" "$etype";;
+                /bin/time -f "$timerf" ./compile.py -B 1 experiment "$size" "$partysize" "$etype";;
             *) # default to FIELD
                 /bin/time -f "$timerf" ./compile.py experiment "$size" "$partysize" "$etype";;
         esac
