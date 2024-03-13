dd38f4553d2bd0b070cb644ad817d454077bae55
Schiller Jonas <q533604@europe.bmw.corp>
2024-03-13 14:59:03 +0100

    Updated without budget

--- host_scripts/measurement.sh
+++ host_scripts/measurement.sh
@@ -57,11 +57,11 @@ cd "$REPO_DIR"
         if [ ! -f "$binarypath" ]; then
         case "$cdomain" in
             RING) 
-                /bin/time -f "$timerf" ./compile.py -Z 3 -R 128 --budget 200000 experiment "$size" "$features" "$partysize" "$etype";;
+                /bin/time -f "$timerf" ./compile.py -Z 3 -R 128 experiment "$size" "$features" "$partysize" "$etype";;
             BINARY) 
                 /bin/time -f "$timerf" ./compile.py -B 32 experiment "$size" "$features" "$partysize" "$etype";;
             *) # default to FIELD
-                /bin/time -f "$timerf" ./compile.py -Y --budget 200000 experiment "$size" "$features" "$partysize" "$etype";;
+                /bin/time -f "$timerf" ./compile.py -Y  experiment "$size" "$features" "$partysize" "$etype";;
         esac
         echo "$(du -BM "$binarypath" | cut -d 'M' -f 1) (Binary file size in MiB)"
         fi
